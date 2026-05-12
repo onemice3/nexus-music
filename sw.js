4ab96b45e7ce788fb60835eb702fef54ffc7353e
onemice3 <135769086+onemice3@users.noreply.github.com>
2026-05-11 22:33:39 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// sw.js
+const CACHE_NAME = 'nexus-cache-v1';
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
